5c7182ebbcc71872d489b59301ada036e5f0d958
rohit <rohit0311@outlook.com>
2020-07-15 10:18:03 +0530

    socket setup

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const express = require('express');
 const path = require('path');
+const http = require('http');
+const socketio = require('socket.io');
+
 
 const app = express();
+const server = http.createServer(app);
+const io = socketio(server);
 
 const port = process.env.PORT || 3000;
 const publicDirectoryPath = path.join(__dirname, '../public')
@@ -9,6 +14,10 @@ const publicDirectoryPath = path.join(__dirname, '../public')
 
 app.use(express.static(publicDirectoryPath))
 
-app.listen(port, () =>{
+io.on('connection',() =>{
+    console.log('new webSocket connection')
+})
+
+server.listen(port, () =>{
     console.log('app listening at port '+ port);
 })
